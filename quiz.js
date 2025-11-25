a64f26aafbdbd734480411aa58fc11ff98bf2682
Qhama Hani <qhamahani@gmail.com>
2025-11-26 00:10:56 +0200

    Select checker

--- quiz.js
+++ quiz.js
@@ -1,15 +1,16 @@
 function checkAnswer (){
-    const correctAnswer = "4"
-const radioButton = document.querySelector("quiz")
+const correctAnswer = "4"
+const radioButton = document.querySelector("input[name="quiz"]:checked")
 const userAnswer = radioButton.value
+
 if (userAnswer === correctAnswer){
-    let feedback = document.getElementById("feedback")
+const feedback = document.getElementById("feedback")
 feedback.textContent("Correct! Well done.")
 } else {
     feedback.textContent("That's incorrect. Try again!")
     
 }
 }
-let submitAnswerButton = document.getElementById("submit-answer")
+const submitAnswerButton = document.getElementById("submit-answer")
 submitAnswerButton.addEventsListener("click", checkAnswer)
 
